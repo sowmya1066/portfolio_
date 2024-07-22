668fdafc0313f3c95078e0cfaa56f2d0609f4db6
Sowmya G S <sowmyags1066@gmail.com>
2024-07-22 19:00:15 +0530

    project card specifications

--- src/Card.jsx
+++ src/Card.jsx
@@ -2,8 +2,25 @@ import htmlImg from "../assets/html.png";
 import ZenList from "../assets/Zenlist.jpg";
 import Shopon from "../assets/shopon.jpg";
 import Alphafinance from "../assets/Alphafinance.jpg";
+import NextFood from "../assets/nextjsFood.png";
+import Wheeltrek from "../assets/wheeltrek.png";
 
 const projects = [
+  {
+    projectName: "Shopon",
+    details: "React, Redux",
+    bgImage: Shopon,
+  },
+  {
+    projectName: "Project 4",
+    details: "Details for Project 3",
+    bgImage: NextFood,
+  },
+  {
+    projectName: "Project 5",
+    details: "Details for Project 3",
+    bgImage: Wheeltrek,
+  },
   {
     projectName: "Alpha Finanace",
     details: "HTML, CSS, JS",
@@ -14,21 +31,7 @@ const projects = [
     details: "JavaScript",
     bgImage: ZenList,
   },
-  {
-    projectName: "Shopon",
-    details: "React, Redux",
-    bgImage: Shopon,
-  },
-  {
-    projectName: "Project 4",
-    details: "Details for Project 3",
-    bgImage: htmlImg,
-  },
-  {
-    projectName: "Project 5",
-    details: "Details for Project 3",
-    bgImage: htmlImg,
-  },
+
   {
     projectName: "Project 6",
     details: "Details for Project 3",
