5f9fab872ea3ceabfac2f7a78891c95a90e7b039
Sowmya G S <sowmyags1066@gmail.com>
2024-07-31 15:38:23 +0530

    external dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ["ldrs"],
+  build: {
+    rollupOptions: {
+      external: [], // Ensure no external dependencies are omitted
+    },
   },
 });
